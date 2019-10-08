a0a9dd184ee499a29d67d558f4eac806536f3613
MannaK77 <56101149+MannaK77@users.noreply.github.com>
2019-10-07 22:02:00 -0500

    Delete mySalesOrder.js

--- mySalesOrder.js
+++
@@ -1,9 +0,0 @@
-
- Order = require ('./order');
- SOrder = require ('./SOrder');
-
- let widget = new SOrderitem('Widget', 2.5, 10);
- let gidget = new SOderitem('Gidget', 1.0,20);
- let order= new SOrder('Bob', 0.10, [widget, gidget]);
-console.log(order.totalvalue());
-
