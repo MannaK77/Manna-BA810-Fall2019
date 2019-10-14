9aecbc720700cc9385a8a9d1a437d23cd0336f1d
MannaK77 <56101149+MannaK77@users.noreply.github.com>
2019-10-14 17:51:06 -0500

    structural changes

---
+++ Node/Javascript/Order.js
@@ -0,0 +1,17 @@
+function Order(product, quantity, price){
+
+let item = {};
+item.product = product;
+item.price = price;
+item.quantity = quantity;
+
+item.value = function(){
+    return item.price * item.quantity;
+    }
+
+     return item;
+}
+
+
+module.exports = Order;
+
